8fd4054bb7095c17c13b737f15d89d9a32ef13e5
Erdogan Tan <124918396+turkishrational@users.noreply.github.com>
2026-04-08 19:05:18 +0300

    Add files via upload

---
+++ scc_2026_win32_samples/HELLO.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define CVALUE 65536
+
+main()
+{
+  int a, b, c, d;
+  a = -1;
+  b = 0;
+  c = CVALUE;
+  d = 1000000;
+  b = a*c; 		
+  printf("Hello, World!\n");
+}
---
+++ scc_2026_win32_samples/hellosccfasm.bat
@@ -0,0 +1,1 @@
+fasm hellosccw.asm HELLOSDCC.EXE
